b8c8a4535b140ab200a00df060a7af0fd18fb256
Saksham Kaushal <skaushal.77@gmail.com>
2019-12-12 16:30:52 +0530

    Tracked required directory structure. Changed gitignore rules.Changed directory name from clusters_temp to clusters_tmp.

--- parameters.h
+++ parameters.h
@@ -7,6 +7,6 @@
 #define PATH_LEN 50			//Path Length Buffer size
 #define INFILE "./fort.200"		//Source filename and location
 #define F_DATA_DIR "./data/"		//Formatted data directory location
-#define OUT_DIR "./clusters_temp/"	//Directory location for storing temporary cluster data
+#define OUT_DIR "./clusters_tmp/"	//Directory location for storing temporary cluster data
 #define CLUSTER_STATS_DIR "./cluster_stats/"	//Directory location for storing number of clusters and their sizes
 #define CLUSTER_LATTICE_DIR "./clusters/"	//Directory location for storing final lattice with cluster labels at corresponding sites
